bb8a59b9ed0c5d3c6a6f5ded1194123c004803e9
Wolfaholic <melissa.mackie@live.com>
2019-12-04 20:06:43 -0800

    finished first challenge, thanks Nicholas for your help

--- index.js
+++ index.js
@@ -39,9 +39,11 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  return {id: id, name: name, email: email}
+  
 }
+ 
 
 /**
  * ### Challenge `getName`
